5438d71e1eeee88a8ccfcdf7b8f7860f762b5a88
zhushengwei <15918660832@139.com>
2016-08-08 15:42:03 +0800

    three

---
+++ projects/pygame_test/test_3D.py
@@ -0,0 +1,67 @@
+# coding:utf-8
+
+import pygame
+from pygame.locals import *
+from sys import exit
+from random import randint
+from gameobjects.vector2 import Vector2
+
+
+class Star(object):
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+
+    clock = pygame.time.Clock()
+
+    stars = []
+
+    for n in xrange(200):
+        x = float(randint(0, 640))
+        y = float(randint(0, 480))
+        speed = float(randint(10, 300))
+        stars.append(Star(x, y, speed))
+
+    white = (255, 255, 255)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+            if event.type == KEYDOWN:
+                exit()
+
+        # 绘制第一颗星星
+        y = float(randint(0, 479))
+        speed = float(randint(10, 300))
+        star = Star(640., y, speed)
+        stars.append(star)
+
+        time_passed = clock.tick()
+        time_passed_seconds = time_passed / 1000.
+
+        screen.fill((0, 0, 0))
+
+        # 绘制所有的星
+        for star in stars:
+            new_x = star.x - time_passed_seconds * star.speed
+            pygame.draw.aaline(screen, white, (new_x, star.y), (star.x + 1., star.y))
+            star.x = new_x
+
+        def on_screen(star):
+            return star.x > 0
+
+        # 星星跑出了画面，就删了它
+        stars = filter(on_screen, stars)
+
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    run()
--- projects/pygame_test/test_ai.py
+++ projects/pygame_test/test_ai.py
@@ -1,29 +1,19 @@
+SCREEN_SIZE = (640, 480)
+NEST_POSITION = (320, 240)
+ANT_COUNT = 20
+NEST_SIZE = 100.
+
+import pygame
+from pygame.locals import *
+from sys import exit
 from gameobjects.vector2 import Vector2
+from random import randint, choice
+
+def run():
+    pygame.init()
 
 
-class StateMachine(object):
-    pass
+    while True:
 
 
-class GameEntity(object):
-    def __init__(self, world, name, image):
-        self.world = world
-        self.name = name
-        self.image = image
-        self.location = Vector2(0, 0)
-        self.destination  = Vector2(0, 0)
-        self.speed = 0
-        self.brain = StateMachine()
-        self.id = 0
-    def render(self, surface):
-        x, y = self.location
-        w, h = self.image.get_size()
-        surface.blit(self.image, (x-w/2, y-h/2))
-    def process(self, time_passed):
-        self.brain.think()
-        if self.speed > 0 and self.location != self.destination:
-            vec_to_destination = self.destination - self.location
-            distance_to_destination = vec_to_destination.get_length()
-            heading = vec_to_destination.get_normalised()
-            travel_distance = min(distance_to_destination, time_passed * self.speed)
-            self.location += travel_distance * heading
+        pygame.display.update()
